2273dc23e250b46c741d90379e3f4d6d3a100aad
brian piercy <bjpcjp@gmail.com>
2017-05-24 19:16:37 -0500

    add hello action & route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # listing 3.4
+  def hello
+  	render html: 'hello world'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # listing 3.5
+  root 'application#hello'
+
 end
