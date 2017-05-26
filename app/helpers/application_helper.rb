14eacdc9f05dffa4e624d36bf809a4542c5bc334
brian piercy <bjpcjp@gmail.com>
2017-05-26 11:07:41 -0500

    added page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	# listing 4.2
+
+	def full_app_title(page_title='')
+		base_title = "Demo"
+		if page_title.empty?
+			base_title
+		else
+			base_title + " | " + page_title
+		end
+	end
+
 end
